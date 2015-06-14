233e12bc3fdeace2223648023a8feed4b6ee449d
Alexandre Ferreira <alexandre.vini.ferreira@gmail.com>
2015-06-14 13:43:22 -0300

    set celery prod settings

--- namesearch/celery.py
+++ namesearch/celery.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 from celery import Celery
 import os
-
 __author__ = 'alexandreferreira'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'namesearch.settings')
+if os.getenv('host') == 'PROD':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'namesearch.settings.prod')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'namesearch.settings')
 
 from django.conf import settings
 
